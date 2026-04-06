6b6eabf0edf0b6daf306104376873f31be6ea45c
Ale007XD <alex.deloverov@gmail.com>
2026-04-06 11:45:40 +0700

    Update run_all.py

--- tests/run_all.py
+++ tests/run_all.py
@@ -1,9 +1,15 @@
 import os
+import sys
 import runpy
 
 
 def run():
     base = os.path.dirname(__file__)
+    root = os.path.dirname(base)
+
+    # ✅ КЛЮЧЕВОЙ ФИКС
+    if root not in sys.path:
+        sys.path.insert(0, root)
 
     for file in os.listdir(base):
         if file.endswith("_test.py"):
