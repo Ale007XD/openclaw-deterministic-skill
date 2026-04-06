c06a1f192202ce4e77707e0f8d3dd2f3b49a946f
Ale007XD <alex.deloverov@gmail.com>
2026-04-06 11:42:33 +0700

    Update run_all.py

--- tests/run_all.py
+++ tests/run_all.py
@@ -1,12 +1,15 @@
 import os
+import runpy
 
 
 def run():
     base = os.path.dirname(__file__)
+
     for file in os.listdir(base):
         if file.endswith("_test.py"):
+            path = os.path.join(base, file)
             print(f"Running {file}")
-            __import__(f"tests.{file[:-3]}")
+            runpy.run_path(path, run_name="__main__")
 
 
 if __name__ == "__main__":
